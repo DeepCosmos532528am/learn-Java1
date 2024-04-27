9db51b7718e290d4987aa0dd6894109bb78d9f22
= <=>
2024-04-27 16:20:52 +0530

    added a program by me
    Signed-off-by: = <=>

--- src/com/compony/gitpushtry.java
+++ src/com/compony/gitpushtry.java
@@ -1,4 +1,32 @@
 package com.compony;
+import java.util.Scanner;
+public class gitpushtry{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
 
-public class gitpushtry {
+        System.out.println("1st Program");
+        String dig1 = sc.next();
+        String dig2 = sc.next();
+        System.out.println();
+        String alpha = " abcdefghijklmnopqrstuvwxyz";
+        int in1 = alpha.indexOf(dig1);
+        int in2 = alpha.indexOf(dig2);
+        System.out.println(in2-in1);
+
+
+        System.out.println("2nd Program");
+        int i = sc.nextInt();
+        char ssc = " abcdefghijklmnopqrstuvwxyz".charAt(i);
+        System.out.println(ssc);
+
+
+        System.out.println("3rd Program");
+        char dig3 = ssc;
+        int in3 = alpha.indexOf(dig3);
+        System.out.println(in3);
+
+
+
+
+    }
 }
