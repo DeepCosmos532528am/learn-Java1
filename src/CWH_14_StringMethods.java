d2c68503bd245f61dbc80dd2ec49efaea2612148
Sachin Sharma <thapak.krishna2125@gmail.com>
2025-04-17 03:30:59 +0530

    ref : Fixed grammatical error and Typos

--- src/CWH_14_StringMethods.java
+++ src/CWH_14_StringMethods.java
@@ -19,17 +19,17 @@ public class CWH_14_StringMethods {
         System.out.println("sachin sharma".length());  //method 3
         //OR
         System.out.println(s.length());  // method 4
-        //NOTE : the four separate types of methods that we have used above and divided them in four different types will be applicable in all the following
-        //types of String Methods , Actually these all four types of  methods gives us the same output , they are just separate ways to take output
+        //NOTE: the four separate types of methods that we have used above and divided them in four different types will be applicable in all the following
+        //types of String Methods, Actually these all four types of methods give us the same output, they are just separate ways to take output
         // we can use any one of them of our choice...
 
 
         //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 2");
-        //Second method is  [ stringVariable.trim() ] this method is used to remove unwanted white space from the String
+        //The Second method is [ stringVariable.trim() ] this method is used to remove unwanted white space from the String
         String s2 = "        sachin sharma      ";
         System.out.println(s2);
-        //here we are printing normally  without the trim method ,so the output will include the white space too
+        //here we are printing normally without the trim method, so the output will include the white space too
 
 
         String trimmedString = s2.trim();  // this will remove the white space from the beginning and from the ending of the string
@@ -37,14 +37,14 @@ public class CWH_14_StringMethods {
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 3 ");
-// Third method is [ stringVariable.toLowerCase() ] - this method is used to convert the string from Upper Case to Lower Case
+//The Third method is [ stringVariable.toLowerCase() ] - this method is used to convert the string from Upper Case to Lower Case
         String originalUpperCase = "SACHIN SHARMA";
         String lowerCaseString = originalUpperCase.toLowerCase();
         System.out.println(lowerCaseString);
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 4 ");
-// Fourth Method is [ stringVariable.toUpperCase() ] - this method is used to convert the string from Lower Case to Upper Case
+//The Fourth Method is [ stringVariable.toUpperCase() ] - this method is used to convert the string from Lower Case to Upper Case
         String originalLowerCase = "sachin sharma";
         String upperCaseString = originalLowerCase.toUpperCase();
 
@@ -53,14 +53,14 @@ public class CWH_14_StringMethods {
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 5");
-// Fifth Method is [ stringVariable.substring(startIndex) ] - this method is used to get the substring starting from a specific index
+//The Fifth Method is [ stringVariable.substring(startIndex) ] - this method is used to get the substring starting from a specific index
         String sentenceForStartOnly = "Sachin Sharma ";
         String subStringFromStart = sentenceForStartOnly.substring(3);
         System.out.println(subStringFromStart);
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 6 ");
-// Sixth Method is [ stringVariable.substring(startIndex, endIndex) ] - this method is used to get a substring between specific start and end indexes
+//The Sixth Method is [ stringVariable.substring(startIndex, endIndex) ] - this method is used to get a substring between specific start and end indexes
 
         String sentenceForStartEnd = "sachin sharma jai shree ram ";
         String subStringRange = sentenceForStartEnd.substring(14, 28);
@@ -68,63 +68,63 @@ public class CWH_14_StringMethods {
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 7");
-// Seventh method is [ stringVariable.replace(target, replacement) ] - this method is used to replace a specific word or character with another
+//The Seventh method is [ stringVariable.replace(target, replacement) ] - this method is used to replace a specific word or character with another
         String replaceOriginal = "sachin sharma ";
         String replacedString = "Sachin sharma".replace("sharma", "musician");
         System.out.println(replacedString);
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 8 ");
-// Eighth Method is [ stringVariable.startsWith("prefix") ] - this method checks if the string starts with the given prefix
+// The Eighth Method is [ stringVariable.startsWith("prefix") ] - this method checks if the string starts with the given prefix
         String checkStart = "Sachin";
         Boolean doesStart = checkStart.startsWith("Sachin");
         System.out.println(doesStart);
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 9");
-// Ninth Method is [ stringVariable.endsWith("suffix") ] - this method checks if the string ends with the given suffix
+//The Ninth Method is [ stringVariable.endsWith("suffix") ] - this method checks if the string ends with the given suffix
         String checkEnd = "Sachin ";
         boolean doesEnd = checkEnd.endsWith("n");
         System.out.println(doesEnd);
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 10");
-// Tenth Method is [ stringVariable.charAt(index) ] - this method returns the character at a specific index
+// The Tenth Method is [ stringVariable.charAt(index) ] - this method returns the character at a specific index
         String charCheck = "sachin sharma ";
         char charAtIndex = checkEnd.charAt(1);
         System.out.println(charAtIndex);
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 11");
-// Eleventh Method is [ stringVariable.indexOf("charOrString") ] - this method returns the index of the first occurrence of the specified character or string
+// The Eleventh Method is [ stringVariable.indexOf("charOrString") ] - this method returns the index of the first occurrence of the specified character or string
         String indexExample = "sachin sharma ";
         int firstIndex = indexExample.lastIndexOf("s");
         System.out.println(firstIndex);
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 12");
-// Twelfth Method is [ stringVariable.indexOf("char", fromIndex) ] - this method returns the index of a character starting search from a given index
+// TheTwelfth Method is [ stringVariable.indexOf("char", fromIndex) ] - this method returns the index of a character starting search from a given index
         String indexWithOffset = "sachin sharma";
         int offsetIndex = indexWithOffset.indexOf("s", 3);
         System.out.println(offsetIndex);
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 13");
-// Thirteenth Method is [ stringVariable.lastIndexOf("char") ] - this method returns the last index of the specified character
+// TheThirteenth Method is [ stringVariable.lastIndexOf("char") ] - this method returns the last index of the specified character
         String lastIndexExample = "sachin sharma";
         int lastIndex = lastIndexExample.lastIndexOf("a");
         System.out.println(lastIndex);
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 14");
-// Fourteenth Method is [ stringVariable.indexOf("char", fromIndex) ] again - used to find character from specific index if it appears multiple times
+// The Fourteenth Method is [ stringVariable.indexOf("char", fromIndex) ] again - used to find character from a specific index if it appears multiple times
         String anotherIndexExample = "sashin shasma";
         int indexWithStart = anotherIndexExample.indexOf("s", 3);
         System.out.println(indexWithStart);
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 15");
-// Fifteenth Method is [ stringVariable.equals("value") ] - this method checks if two strings are exactly equal (case-sensitive)
+// The Fifteenth Method is [ stringVariable.equals("value") ] - this method checks if two strings are exactly equal (case-sensitive)
         String matchExact = "sachin sharma";
         boolean isEqual = matchExact.equals("sachin sharma");
         System.out.println(isEqual);
@@ -133,14 +133,14 @@ public class CWH_14_StringMethods {
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         System.out.println("no. 16");
-// Sixteenth Method is [ stringVariable.equalsIgnoreCase("value") ] - this method checks if two strings are equal ignoring case
+// The Sixteenth Method is [ stringVariable.equalsIgnoreCase("value") ] - this method checks if two strings are equal ignoring the case
         String ignoreCaseMatch = "sachin sharma";
         boolean isEqualIgnoreCase = ignoreCaseMatch.equalsIgnoreCase("SaChIn ShArma");
         System.out.println(isEqualIgnoreCase);
-// this method is not Case Sensitive method ,it only focuses on the alphabets instead of also focusing on Cases
+// this method is not the Case Sensitive method, it only focuses on the alphabets instead of also focusing on Cases
 
 
-        System.out.println("no. 17");// Seventeenth method is [ stringVariable.compareTo( )] this method is used to compare two strings lexicographically. It is similar to the equals() method but the thing that is special in it is that it stops as soon as a difference is found and returns a positive, negative, or zero value based on the comparison. Lexicographical means comparing strings based on the dictionary order of their characters.
+        System.out.println("no. 17");// The Seventeenth method is [ stringVariable.compareTo( )] this method is used to compare two strings lexicographically. It is similar to the equals() method but the thing that is special in it is that it stops as soon as a difference is found and returns a positive, negative, or zero value based on the comparison. Lexicographical means comparing strings based on the dictionary order of their characters.
 
         String s17_1 = "Sachin Sharma";
         String s17_2 = "Sachin sharma";
@@ -153,7 +153,7 @@ public class CWH_14_StringMethods {
 // The comparison stops as soon as the first difference is found.
 
 
-        //                 </ Here Our all 17 methods of Strings has now been Finished />
+        //                 </ Here Our all 17 methods of Strings have now been Finished />
 
 
         //   The escape sequence characters
